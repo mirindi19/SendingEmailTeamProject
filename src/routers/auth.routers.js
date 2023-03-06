f0e553014423cb9051cea93434e7050e304a8e4e
Norbert Ishimwe <ishimwe96@gmail.com>
2023-03-06 03:21:09 +0200

    resolved conflicts

--- src/routers/auth.routers.js
+++ src/routers/auth.routers.js
@@ -1,3 +1,7 @@
+import { Router } from "express";
+import authController from"../controller/authController";
+import checkUser from "../middelwares/checkUser";
+const router = Router();
 
 router.post("/signup",authController.creatAccount);
 router.post("/login",checkUser,authController.Login);
